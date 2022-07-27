29095d7bf3b871e208fcb4722f8cfbe6fdc0d19d
Soyon <soyonchoi29@gmail.com>
2022-07-27 17:00:05 -0400

    hand tracking system implemented

--- main.py
+++ main.py
@@ -8,37 +8,71 @@ import mediapipe as mp
 import svm
 
 
+class HandTracker:
+
+    def __init__(self, mode=False, max_hands=2, detection_con=0.5, model_complexity=1, track_con=0.5):
+        self.results = None
+        self.mode = mode
+        self.maxHands = max_hands
+        self.detectionCon = detection_con
+        self.modelComplex = model_complexity
+        self.trackCon = track_con
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(static_image_mode=self.mode,
+                                        max_num_hands=self.maxHands,
+                                        model_complexity=self.modelComplex,
+                                        min_detection_confidence=self.detectionCon,
+                                        min_tracking_confidence=self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def find_hands(self, img, draw=True):
+
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+
+        return img
+
+    def find_positions(self, img, hand_num=0, draw=True):
+
+        lmlist = []
+
+        if self.results.multi_hand_landmarks:
+            hand = self.results.multi_hand_landmarks[hand_num]
+
+            for id, lm in enumerate(hand.landmark):
+                    h, w, c = img.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    lmlist.append([id, cx, cy])
+            if draw:
+                cv2.circle(img, (cx, cy), 3, (255, 0, 255), cv2.FILLED)
+
+        return lmlist
+
+
 if __name__ == '__main__':
     cap = cv2.VideoCapture(0)
-
-    mpHands = mp.solutions.hands
-    hands = mpHands.Hands(static_image_mode=False,
-                          max_num_hands=2,
-                          min_detection_confidence=0.5,
-                          min_tracking_confidence=0.5)
-    mpDraw = mp.solutions.drawing_utils
+    tracker = HandTracker()
 
     pTime = 0
     cTime = 0
 
     while True:
         success, img = cap.read()
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = tracker.find_hands(img)
+        lmList = tracker.find_positions(img)
 
-        results = hands.process(imgRGB)
-        if results.multi_hand_landmarks:
-            for handLms in results.multi_hand_landmarks:
-                for id, lm in enumerate(handLms.landmark):
-                    h, w, c = img.shape
-                    cx, cy = int(lm.x * w), int(lm.y * h)
-                    cv2.circle(img, (cx, cy), 3, (255, 0, 255), cv2.FILLED)
-
-                mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+        if len(lmList) != 0:
+            print(lmList[4])
 
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
 
-        cv2.putText(img, f'FPS:{int(fps)}', (20,70), cv2.FONT_HERSHEYSIMPLEX, 1, (0, 255, 0), 2)
+        cv2.putText(img, f'FPS:{int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         cv2.imshow("Signed English Translator", img)
         cv2.waitKey(1)
