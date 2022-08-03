5ea5824d55419741c0a75e329a7d2555db453de2
Soyon <soyonchoi29@gmail.com>
2022-08-03 01:19:36 -0400

    why does the hand detector give diff coordinates when a single pic is put through?

---
+++ test.py
@@ -0,0 +1,58 @@
+import numpy as np
+import cv2
+import os
+from skimage.io import imread
+from skimage.transform import resize
+import matplotlib.pyplot as plt
+
+import svm2
+import handTracker
+
+if __name__ == '__main__':
+
+    img = imread('C:/Users/soyon/Documents/Codes/ASL-Translator/dataset/train/H/H1166.jpg')
+    letters = sorted(os.listdir('C:/Users/soyon/Documents/Codes/ASL-Translator/dataset/train'))
+
+    # plt.imshow(img)
+    # plt.show()
+
+    tracker = handTracker.HandTracker(max_hands=1)
+    img = tracker.find_hands(img)
+
+    plt.imshow(img)
+    plt.show()
+
+    model = svm2.SVM()
+    pca = svm2.Data()
+    loaded_model = model.load_model('svm_model_no_pca_world.sav')
+    # loaded_pca = pca.load_pca('pca_6_world.sav')
+
+    lmlist = tracker.find_positions(img)
+    # print(lmlist)
+    xlist = np.array(lmlist[:, 2])
+    # print(xlist)
+    ylist = np.array(lmlist[:, 3])
+    # print(ylist)
+
+    xylist = []
+    for cx in xlist:
+        xylist.append(cx)
+    for cy in ylist:
+        xylist.append(cy)
+
+    print(xylist)
+    pos = np.array(xylist)
+    pos = pos.reshape(1, -1)
+
+    pos_pca = loaded_pca.transform(pos)
+    # pos_pca = loaded_pca.inverse_transform(pos_pca)
+
+    predicted_letter = loaded_model.predict(pos_pca)
+    predicted_letter = letters[int(predicted_letter)]
+    print(predicted_letter)
+
+    probability = np.ravel(loaded_model.predict_proba(pos_pca))
+    # print(probability)
+    probability = max(probability) * 100
+    print('prob = ', probability)
+
