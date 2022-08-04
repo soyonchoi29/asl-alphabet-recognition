9d87be5f3c31fdffaf96b5b807a52b4a242d6095
Soyon <soyonchoi29@gmail.com>
2022-08-04 16:30:16 -0400

    plot and compare top two PCs

---
+++ compare.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import os
+
+if __name__ == '__main__':
+
+    X_pca_kaggle = pickle.load(open('X_pca_kaggle.sav', 'rb'))
+    y_kaggle = pickle.load(open('y_kaggle.sav', 'rb'))
+
+    X_pca_webcam = pickle.load(open('X_pca_webcam.sav', 'rb'))
+    y_webcam = pickle.load(open('y_webcam.sav', 'rb'))
+
+    imgdir = 'C:/Users/soyon/Documents/Codes/ASL-Translator/dataset/train'
+    letters = sorted(os.listdir(imgdir))
+
+    markers = []
+    for letter in letters:
+        markers.append('${}$'.format(letter))
+
+    # [red, blue]
+    colors = ['#de3838', '#007bc3']
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+
+    # plot kaggle data (#pc = 2)
+    for l, m in zip(np.unique(y_kaggle), markers):
+        plt.scatter(X_pca_kaggle[y_kaggle == l, 0], X_pca_kaggle[y_kaggle == l, 1],
+                    c=colors[0], marker=m, label=l)
+
+    # plot webcam data on same plot
+    for l, m in zip(np.unique(y_webcam), markers):
+        plt.scatter(X_pca_webcam[y_webcam == l, 0], X_pca_webcam[y_webcam == l, 1],
+                    c=colors[1], marker=m, label=l)
+
+    plt.xlabel('PC 1')
+    plt.ylabel('PC 2')
+    # plt.legend()
+    plt.show()
+
