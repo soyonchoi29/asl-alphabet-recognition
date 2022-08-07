74dcd575b922839af77d892f46f28d0696e40219
Soyon <soyonchoi29@gmail.com>
2022-08-06 20:04:06 -0400

    compared all wanted tsne plots :)

--- compare.py
+++ compare.py
@@ -5,10 +5,10 @@ import os
 
 if __name__ == '__main__':
 
-    X_pca_kaggle = pickle.load(open('X_tsne_kaggle.sav', 'rb'))
+    X_kaggle = pickle.load(open('X_tsne_kaggle.sav', 'rb'))
     y_kaggle = pickle.load(open('y_tsne_kaggle.sav', 'rb'))
 
-    X_pca_webcam = pickle.load(open('X_tsne_webcam.sav', 'rb'))
+    X_webcam = pickle.load(open('X_tsne_webcam.sav', 'rb'))
     y_webcam = pickle.load(open('y_tsne_webcam.sav', 'rb'))
 
     imgdir = 'C:/Users/soyon/Documents/Codes/ASL-Translator/dataset/train'
@@ -26,11 +26,11 @@ if __name__ == '__main__':
 
     # plot kaggle data (#pc = 2)
     for l, m in zip(np.unique(y_kaggle), markers):
-        plt.scatter(X_pca_kaggle[y_kaggle == l, 0], X_pca_kaggle[y_kaggle == l, 1], c=colors[0], marker=m, label=l)
+        plt.scatter(X_kaggle[y_kaggle == l, 0], X_kaggle[y_kaggle == l, 1], c=colors[0], marker=m, label=l)
 
     # plot webcam data on same plot
     for l, m in zip(np.unique(y_webcam), markers):
-        plt.scatter(X_pca_webcam[y_webcam == l, 0], X_pca_webcam[y_webcam == l, 1], c=colors[1], marker=m, label=l)
+        plt.scatter(X_webcam[y_webcam == l, 0], X_webcam[y_webcam == l, 1], c=colors[1], marker=m, label=l)
 
     # # plot kaggle data (#pc = 3)
     # for l, m in zip(np.unique(y_kaggle), markers):
@@ -46,8 +46,8 @@ if __name__ == '__main__':
     #                X_pca_webcam[y_webcam == l, 2],
     #                c=colors[1], marker=m, label=l)
 
-    plt.xlabel('PC 1')
-    plt.ylabel('PC 2')
+    plt.xlabel('Comp 1')
+    plt.ylabel('Comp 2')
     # ax.set_xlabel('PC 1')
     # ax.set_ylabel('PC 2')
     # ax.set_zlabel('PC 3')
